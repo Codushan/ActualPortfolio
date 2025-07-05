79c44e5cd3f391453cd18ade03df399451b30cfd
CB <chandrabhushankumar138@gmail.com>
2025-07-05 22:16:54 +0530

    stats added4

--- app/Main/layout.tsx
+++ app/Main/layout.tsx
@@ -6,9 +6,8 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Portfolio Website",
-  description: "A portfolio website with multiple themes and profiles",
-    generator: 'v0.dev'
+  title: "Chandrabhushan Portfolio",
+  description: "A portfolio website with multiple themes and profiles"
 }
 
 export default function MainLayout({
